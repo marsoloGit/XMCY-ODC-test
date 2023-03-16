28633f0d8ab40e91eca39721df7a4542c5eca5de
marsoloGit <marsoloGit@users.noreply.github.com>
2023-03-16 13:29:48 +0100

    pytest

--- ui/selenium/pages/home_page.py
+++ ui/selenium/pages/home_page.py
@@ -1,6 +1,7 @@
 from ui.selenium.elements.common_elements import Element
 from ui.selenium.pages.base_bage import BasePage
 from ui.selenium.pages.research_and_education_page import ResearchEducationPage
+from config import settings as s
 
 
 class HomePage(BasePage):
@@ -8,5 +9,6 @@ class HomePage(BasePage):
     lnk_research_and_education = Element(link_text="RESEARCH & EDUCATION")
 
     def navigate_to_research_and_education(self):
+        self.w.set_script_timeout(s.WEBDRIVER_ELEMENT_WAIT)
         self.lnk_research_and_education.click()
         return ResearchEducationPage(self.w)
